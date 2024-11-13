70daa3c4c1bfc11c74f90d40b8224c23f9985248
t0xicVybez <t0xttv23@gmail.com>
2024-11-12 20:13:38 -0800

    optmized client.lua
    Removed unused variables and functions, keeping only the necessary Config table.
    Simplified the structure by directly returning the Config table instead of using a local variable.
    Removed unnecessary comments and whitespace for a more compact and efficient file.

---
+++ client/configs/client.lua
@@ -0,0 +1,7 @@
+return {
+    Distance = {
+        SecondaryRamp = 2.8, -- if the vehicle is getting towed to the top, this is the distance from the rope it stops.
+        MainRamp = 2.5,      -- if the vehicle is getting towed to the bottom, this is the distance from the rope it stops.
+    },
+    Debug = true, -- enables /spawntowtruck command for testing.
+}
